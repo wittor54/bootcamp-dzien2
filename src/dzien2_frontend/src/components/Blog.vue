c611a7ca3c10aa07bf9c2f0bf545838828300753
wittor54 <xdwittor@gmail.com>
2024-06-25 19:59:54 +0200

    wiecej dodawania wpisow

--- src/dzien2_frontend/src/components/Blog.vue
+++ src/dzien2_frontend/src/components/Blog.vue
@@ -1,5 +1,32 @@
 <template>
     <div>
+        <button @click="pobierzWpisy">odczytaj wpisy</button>
         siema blog!
+        {{wpisy}}
+        <input v-model="nowyBlog" type="text">
+        <button @click="dodajWpisy">dodaj</button>
     </div>
 </template>
+
+<script>
+import { dzien2_backend } from "declarations/dzien2_backend/index";
+
+export default {
+    data() {
+        return {
+            wpisy: [],
+            nowyBlog: ""
+        }
+    },
+    methods: {
+        async dodajWpisy(){
+            await dzien2_backend.dodaj_wpis(this.nowyBlog);
+        },
+        async pobierzWpisy(){
+            this.wpisy = await dzien2_backend.odczytaj_wpisy();
+        }
+    },
+
+}
+</script>
+
